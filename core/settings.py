1f3080e2d7da4270dfe6b17ab495b77066d75e80
Gabriel Alves Silva <ggalvesilva63@gmail.com>
2024-12-19 15:36:18 -0300

    Altered database PORT to integer and populated static folder with style.css

--- core/settings.py
+++ core/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': 5432,
     }
 }
 
